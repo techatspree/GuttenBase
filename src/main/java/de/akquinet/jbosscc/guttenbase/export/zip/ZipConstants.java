51e49a4b4607d2469f8b0255da5b411facb50eb5
Markus Dahm <markus.dahm@akquinet.de>
2012-10-30 16:55:38 +0100

    fixed

--- src/main/java/de/akquinet/jbosscc/guttenbase/export/zip/ZipConstants.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/export/zip/ZipConstants.java
@@ -10,20 +10,20 @@ package de.akquinet.jbosscc.guttenbase.export.zip;
  * @author M. Dahm
  */
 public interface ZipConstants {
-	String DBINFO_NAME = "DB-INFO.txt";
-	String TABLE_INFO_NAME = "TABLE-INFO.txt";
-	String COLUMN_NAME = "COLUMNS";
-	String INDEX_NAME = "INDEXES";
-	String GUTTEN_BASE_NAME = "GuttenBase";
-	String TABLE_DATA_NAME = "DATA";
-	String PATH_SEPARATOR = "/";
-	String METADATA_NAME = "METADATA";
-	String EXTRA_INFO_NAME = "EXTRA-INFO";
+  String DBINFO_NAME = "DB-INFO.txt";
+  String TABLE_INFO_NAME = "TABLE-INFO.txt";
+  String COLUMN_NAME = "COLUMNS";
+  String INDEX_NAME = "INDEXES";
+  String GUTTEN_BASE_NAME = "GuttenBase";
+  String TABLE_DATA_NAME = "DATA";
+  char PATH_SEPARATOR = '/';
+  String METADATA_NAME = "METADATA";
+  String EXTRA_INFO_NAME = "EXTRA-INFO";
 
-	String PREFIX = GUTTEN_BASE_NAME + PATH_SEPARATOR;
+  String PREFIX = GUTTEN_BASE_NAME + PATH_SEPARATOR;
 
-	String META_DATA = PREFIX + METADATA_NAME;
-	String EXTRA_INFO = PREFIX + EXTRA_INFO_NAME;
-	String META_INF = "META-INF";
-	String MANIFEST_NAME = META_INF + PATH_SEPARATOR + "MANIFEST.MF";
+  String META_DATA = PREFIX + METADATA_NAME;
+  String EXTRA_INFO = PREFIX + EXTRA_INFO_NAME;
+  String META_INF = "META-INF";
+  String MANIFEST_NAME = META_INF + PATH_SEPARATOR + "MANIFEST.MF";
 }
