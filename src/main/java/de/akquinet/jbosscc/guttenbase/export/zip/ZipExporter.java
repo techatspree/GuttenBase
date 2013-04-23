a6e819dce0ad38a90c2b9fa98652af97374e776e
Markus Dahm <markus.dahm@akquinet.de>
2013-04-23 10:26:48 +0200

    add resources to export jar

--- src/main/java/de/akquinet/jbosscc/guttenbase/export/zip/ZipExporter.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/export/zip/ZipExporter.java
@@ -5,15 +5,18 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintStream;
 import java.io.Serializable;
+import java.net.URL;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+import org.apache.log4j.Logger;
 
 import de.akquinet.jbosscc.guttenbase.export.ExportDumpConnectorInfo;
 import de.akquinet.jbosscc.guttenbase.export.ExportDumpExtraInformation;
@@ -42,220 +45,239 @@ import de.akquinet.jbosscc.guttenbase.utils.Util;
  * @author M. Dahm
  */
 public class ZipExporter implements Exporter {
-	private ZipOutputStream _zipOutputStream;
-	private ObjectOutputStream _objectOutputStream;
-	private File _tempFile;
-	private ConnectorRepository _connectorRepository;
-	private String _connectorId;
-	private ExportDumpConnectorInfo _exportDumpConnectionInfo;
+  private static final Logger LOG = Logger.getLogger(ZipExporter.class);
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void initializeExport(final ConnectorRepository connectorRepository, final String connectorId,
-			final ExportDumpConnectorInfo exportDumpConnectionInfo) throws IOException {
-		assert exportDumpConnectionInfo != null : "exportDumpConnectionInfo != null";
-		assert connectorId != null : "connectorId != null";
-		assert connectorRepository != null : "connectorRepository != null";
+  private ZipOutputStream _zipOutputStream;
+  private ObjectOutputStream _objectOutputStream;
+  private File _tempFile;
+  private ConnectorRepository _connectorRepository;
+  private String _connectorId;
+  private ExportDumpConnectorInfo _exportDumpConnectionInfo;
 
-		_connectorRepository = connectorRepository;
-		_connectorId = connectorId;
-		_exportDumpConnectionInfo = exportDumpConnectionInfo;
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void initializeExport(final ConnectorRepository connectorRepository, final String connectorId,
+      final ExportDumpConnectorInfo exportDumpConnectionInfo) throws IOException {
+    assert exportDumpConnectionInfo != null : "exportDumpConnectionInfo != null";
+    assert connectorId != null : "connectorId != null";
+    assert connectorRepository != null : "connectorRepository != null";
 
-		final File file = new File(exportDumpConnectionInfo.getPath());
-		final FileOutputStream fos = new FileOutputStream(file);
-		_zipOutputStream = new ZipOutputStream(fos);
+    _connectorRepository = connectorRepository;
+    _connectorId = connectorId;
+    _exportDumpConnectionInfo = exportDumpConnectionInfo;
 
-		final ZipExporterClassResources zipExporterClassResources = connectorRepository.getConnectorHint(connectorId,
-				ZipExporterClassResources.class).getValue();
-		addClassesToJar(connectorRepository, connectorId, zipExporterClassResources);
+    final File file = new File(exportDumpConnectionInfo.getPath());
+    final FileOutputStream fos = new FileOutputStream(file);
+    _zipOutputStream = new ZipOutputStream(fos);
 
-		writeManifestEntry(zipExporterClassResources);
-	}
+    final ZipExporterClassResources zipExporterClassResources = connectorRepository.getConnectorHint(connectorId,
+        ZipExporterClassResources.class).getValue();
+    addClassesToJar(connectorRepository, connectorId, zipExporterClassResources);
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void finishExport() throws Exception {
-		writeExtraInformation();
+    writeManifestEntry(zipExporterClassResources);
+  }
 
-		_zipOutputStream.close();
-		_zipOutputStream = null;
-	}
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void finishExport() throws Exception {
+    writeExtraInformation();
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void writeDatabaseMetaData(final DatabaseMetaData databaseMetaData) throws IOException {
-		writeDatabaseEntry(databaseMetaData);
+    final ZipExporterClassResources zipExporterClassResources = _connectorRepository.getConnectorHint(_connectorId,
+        ZipExporterClassResources.class).getValue();
+    addResourcesToJar(zipExporterClassResources);
+    _zipOutputStream.close();
+    _zipOutputStream = null;
+  }
 
-		for (final TableMetaData tableMetaData : databaseMetaData.getTableMetaData()) {
-			writeTableEntry(tableMetaData);
-			writeColumnEntries(tableMetaData);
-			writeIndexEntries(tableMetaData);
-		}
-	}
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void writeDatabaseMetaData(final DatabaseMetaData databaseMetaData) throws IOException {
+    writeDatabaseEntry(databaseMetaData);
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * Does nothing.
-	 */
-	@Override
-	public void writeTableHeader(final ExportTableHeader exportTableHeader) throws IOException {
-	}
+    for (final TableMetaData tableMetaData : databaseMetaData.getTableMetaData()) {
+      writeTableEntry(tableMetaData);
+      writeColumnEntries(tableMetaData);
+      writeIndexEntries(tableMetaData);
+    }
+  }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * Open new ZIP entry. Data will be written to a temporary file first, because otherwise it may exceed the memory.
-	 */
-	@Override
-	public void initializeWriteTableData(final TableMetaData tableMetaData) throws IOException {
-		newEntry(ZipConstants.PREFIX + tableMetaData.getTableName() + ZipConstants.PATH_SEPARATOR + ZipConstants.TABLE_DATA_NAME);
+  /**
+   * {@inheritDoc}
+   * 
+   * Does nothing.
+   */
+  @Override
+  public void writeTableHeader(final ExportTableHeader exportTableHeader) throws IOException {
+  }
 
-		_tempFile = File.createTempFile("GB-JAR-", null);
-		_tempFile.deleteOnExit();
+  /**
+   * {@inheritDoc}
+   * 
+   * Open new ZIP entry. Data will be written to a temporary file first, because otherwise it may exceed the memory.
+   */
+  @Override
+  public void initializeWriteTableData(final TableMetaData tableMetaData) throws IOException {
+    newEntry(ZipConstants.PREFIX + tableMetaData.getTableName() + ZipConstants.PATH_SEPARATOR + ZipConstants.TABLE_DATA_NAME);
 
-		_objectOutputStream = new ObjectOutputStream(new FileOutputStream(_tempFile));
-	}
+    _tempFile = File.createTempFile("GB-JAR-", null);
+    _tempFile.deleteOnExit();
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * Close current ZIP entry.
-	 */
-	@Override
-	public void finalizeWriteTableData(final TableMetaData table) throws IOException {
-		_objectOutputStream.close();
+    _objectOutputStream = new ObjectOutputStream(new FileOutputStream(_tempFile));
+  }
 
-		final FileInputStream fis = new FileInputStream(_tempFile);
-		Util.copy(fis, _zipOutputStream);
-		closeEntry();
+  /**
+   * {@inheritDoc}
+   * 
+   * Close current ZIP entry.
+   */
+  @Override
+  public void finalizeWriteTableData(final TableMetaData table) throws IOException {
+    _objectOutputStream.close();
 
-		_tempFile.delete();
-		_tempFile = null;
-		_objectOutputStream = null;
-	}
+    final FileInputStream fis = new FileInputStream(_tempFile);
+    Util.copy(fis, _zipOutputStream);
+    closeEntry();
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * Does nothing.
-	 */
-	@Override
-	public void initializeWriteRowData(final TableMetaData table) {
-	}
+    _tempFile.delete();
+    _tempFile = null;
+    _objectOutputStream = null;
+  }
 
-	/**
-	 * {@inheritDoc}
-	 * 
-	 * Does nothing.
-	 */
-	@Override
-	public void finalizeWriteRowData(final TableMetaData table) {
-	}
+  /**
+   * {@inheritDoc}
+   * 
+   * Does nothing.
+   */
+  @Override
+  public void initializeWriteRowData(final TableMetaData table) {
+  }
 
-	/**
-	 * Resets the output stream which reduces memory foot print drastically. See {@link ObjectOutputStream#reset()} for details.
-	 */
-	@Override
-	public void flush() throws IOException {
-		if (_objectOutputStream != null) {
-			_objectOutputStream.reset();
-			_objectOutputStream.flush();
-		}
-	}
+  /**
+   * {@inheritDoc}
+   * 
+   * Does nothing.
+   */
+  @Override
+  public void finalizeWriteRowData(final TableMetaData table) {
+  }
 
-	@Override
-	public void writeObject(final Object obj) throws IOException {
-		_objectOutputStream.writeObject(obj);
-	}
+  /**
+   * Resets the output stream which reduces memory foot print drastically. See {@link ObjectOutputStream#reset()} for details.
+   */
+  @Override
+  public void flush() throws IOException {
+    if (_objectOutputStream != null) {
+      _objectOutputStream.reset();
+      _objectOutputStream.flush();
+    }
+  }
 
-	private void writeIndexEntries(final TableMetaData tableMetaData) throws IOException {
-		final String indexPath = ZipConstants.PREFIX + tableMetaData.getTableName() + ZipConstants.PATH_SEPARATOR + ZipConstants.INDEX_NAME
-				+ ZipConstants.PATH_SEPARATOR;
+  @Override
+  public void writeObject(final Object obj) throws IOException {
+    _objectOutputStream.writeObject(obj);
+  }
 
-		for (final IndexMetaData indexMetaData : tableMetaData.getIndexes()) {
-			newEntry(indexPath + indexMetaData.getIndexName() + ".txt");
-			new ZipIndexMetaDataWriter().writeIndexMetaDataEntry(indexMetaData).store("Index meta data", _zipOutputStream);
-			closeEntry();
-		}
-	}
+  private void writeIndexEntries(final TableMetaData tableMetaData) throws IOException {
+    final String indexPath = ZipConstants.PREFIX + tableMetaData.getTableName() + ZipConstants.PATH_SEPARATOR + ZipConstants.INDEX_NAME
+        + ZipConstants.PATH_SEPARATOR;
 
-	private void writeColumnEntries(final TableMetaData tableMetaData) throws IOException {
-		final String columnPath = ZipConstants.PREFIX + tableMetaData.getTableName() + ZipConstants.PATH_SEPARATOR + ZipConstants.COLUMN_NAME
-				+ ZipConstants.PATH_SEPARATOR;
+    for (final IndexMetaData indexMetaData : tableMetaData.getIndexes()) {
+      newEntry(indexPath + indexMetaData.getIndexName() + ".txt");
+      new ZipIndexMetaDataWriter().writeIndexMetaDataEntry(indexMetaData).store("Index meta data", _zipOutputStream);
+      closeEntry();
+    }
+  }
 
-		for (final ColumnMetaData columnMetaData : tableMetaData.getColumnMetaData()) {
-			newEntry(columnPath + columnMetaData.getColumnName() + ".txt");
-			new ZipColumnMetaDataWriter().writeColumnMetaDataEntry(columnMetaData).store("Column meta data", _zipOutputStream);
-			closeEntry();
-		}
-	}
+  private void writeColumnEntries(final TableMetaData tableMetaData) throws IOException {
+    final String columnPath = ZipConstants.PREFIX + tableMetaData.getTableName() + ZipConstants.PATH_SEPARATOR + ZipConstants.COLUMN_NAME
+        + ZipConstants.PATH_SEPARATOR;
 
-	private void writeTableEntry(final TableMetaData tableMetaData) throws IOException {
-		newEntry(ZipConstants.PREFIX + tableMetaData.getTableName() + ZipConstants.PATH_SEPARATOR + ZipConstants.TABLE_INFO_NAME);
-		new ZipTableMetaDataWriter().writeTableMetaDataEntry(tableMetaData).store("Table meta data", _zipOutputStream);
-		closeEntry();
-	}
+    for (final ColumnMetaData columnMetaData : tableMetaData.getColumnMetaData()) {
+      newEntry(columnPath + columnMetaData.getColumnName() + ".txt");
+      new ZipColumnMetaDataWriter().writeColumnMetaDataEntry(columnMetaData).store("Column meta data", _zipOutputStream);
+      closeEntry();
+    }
+  }
 
-	private void writeDatabaseEntry(final DatabaseMetaData databaseMetaData) throws IOException {
-		newEntry(ZipConstants.PREFIX + ZipConstants.DBINFO_NAME);
-		new ZipDatabaseMetaDataWriter().writeDatabaseMetaDataEntry(databaseMetaData).store("Database meta data", _zipOutputStream);
-		closeEntry();
+  private void writeTableEntry(final TableMetaData tableMetaData) throws IOException {
+    newEntry(ZipConstants.PREFIX + tableMetaData.getTableName() + ZipConstants.PATH_SEPARATOR + ZipConstants.TABLE_INFO_NAME);
+    new ZipTableMetaDataWriter().writeTableMetaDataEntry(tableMetaData).store("Table meta data", _zipOutputStream);
+    closeEntry();
+  }
 
-		newEntry(ZipConstants.META_DATA);
-		_zipOutputStream.write(Util.toByteArray(databaseMetaData));
-		closeEntry();
-	}
+  private void writeDatabaseEntry(final DatabaseMetaData databaseMetaData) throws IOException {
+    newEntry(ZipConstants.PREFIX + ZipConstants.DBINFO_NAME);
+    new ZipDatabaseMetaDataWriter().writeDatabaseMetaDataEntry(databaseMetaData).store("Database meta data", _zipOutputStream);
+    closeEntry();
 
-	private void newEntry(final String name) throws IOException {
-		_zipOutputStream.putNextEntry(new ZipEntry(name));
-	}
+    newEntry(ZipConstants.META_DATA);
+    _zipOutputStream.write(Util.toByteArray(databaseMetaData));
+    closeEntry();
+  }
 
-	private void closeEntry() throws IOException {
-		_zipOutputStream.closeEntry();
-	}
+  private void newEntry(final String name) throws IOException {
+    _zipOutputStream.putNextEntry(new ZipEntry(name));
+  }
 
-	private void addClassesToJar(final ConnectorRepository connectorRepository, final String connectorId,
-			final ZipExporterClassResources zipExporterClassResources) throws IOException {
-		final ZipClassesFromClassResourceExporter zipClassesFromClassResourceExporter = new ZipClassesFromClassResourceExporter(
-				_zipOutputStream);
+  private void closeEntry() throws IOException {
+    _zipOutputStream.closeEntry();
+  }
 
-		final List<Class<?>> classResources = zipExporterClassResources.getClassResources();
-		for (final Class<?> clazz : classResources) {
-			zipClassesFromClassResourceExporter.copyClassesToZip(clazz);
-		}
-	}
+  private void addClassesToJar(final ConnectorRepository connectorRepository, final String connectorId,
+      final ZipExporterClassResources zipExporterClassResources) throws IOException {
+    final ZipClassesFromClassResourceExporter zipClassesFromClassResourceExporter = new ZipClassesFromClassResourceExporter(
+        _zipOutputStream);
 
-	private void writeManifestEntry(final ZipExporterClassResources zipExporterClassResources) throws IOException {
-		newEntry(ZipConstants.MANIFEST_NAME);
-		final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		final PrintStream printStream = new PrintStream(bos);
-		printStream.println("Manifest-Version: 1.0");
-		printStream.println("Created-By: GuttenBase ZIP Exporter");
-		printStream.println("Main-Class: " + zipExporterClassResources.getStartupClass().getName());
-		printStream.close();
-		_zipOutputStream.write(bos.toByteArray());
-		closeEntry();
-	}
+    for (final Class<?> clazz : zipExporterClassResources.getClassResources()) {
+      zipClassesFromClassResourceExporter.copyClassesToZip(clazz);
+    }
+  }
 
-	private void writeExtraInformation() throws SQLException, IOException {
-		final ExportDumpExtraInformation exportDumpExtraInformation = _connectorRepository.getConnectorHint(_connectorId,
-				ExportDumpExtraInformation.class).getValue();
+  private void addResourcesToJar(final ZipExporterClassResources zipExporterClassResources) throws IOException {
+    final ZipResourceExporter zipResourceExporter = new ZipResourceExporter(_zipOutputStream);
 
-		final Map<String, Serializable> extraInformation = exportDumpExtraInformation.getExtraInformation(_connectorRepository, _connectorId,
-				_exportDumpConnectionInfo);
+    for (final Entry<String, URL> entry : zipExporterClassResources.getUrlResources().entrySet()) {
+      final URL url = entry.getValue();
 
-		for (final Entry<String, Serializable> entry : extraInformation.entrySet()) {
-			newEntry(ZipConstants.EXTRA_INFO + ZipConstants.PATH_SEPARATOR + entry.getKey());
-			final byte[] byteArray = Util.toByteArray(entry.getValue());
-			_zipOutputStream.write(byteArray);
-			closeEntry();
-		}
-	}
+      if (url != null) {
+        final InputStream stream = url.openStream();
+        zipResourceExporter.addEntry(entry.getKey(), stream);
+      } else {
+        LOG.warn("Could not add null URL content ");
+      }
+    }
+  }
+
+  private void writeManifestEntry(final ZipExporterClassResources zipExporterClassResources) throws IOException {
+    newEntry(ZipConstants.MANIFEST_NAME);
+    final ByteArrayOutputStream bos = new ByteArrayOutputStream();
+    final PrintStream printStream = new PrintStream(bos);
+    printStream.println("Manifest-Version: 1.0");
+    printStream.println("Created-By: GuttenBase ZIP Exporter");
+    printStream.println("Main-Class: " + zipExporterClassResources.getStartupClass().getName());
+    printStream.close();
+    _zipOutputStream.write(bos.toByteArray());
+    closeEntry();
+  }
+
+  private void writeExtraInformation() throws SQLException, IOException {
+    final ExportDumpExtraInformation exportDumpExtraInformation = _connectorRepository.getConnectorHint(_connectorId,
+        ExportDumpExtraInformation.class).getValue();
+
+    final Map<String, Serializable> extraInformation = exportDumpExtraInformation.getExtraInformation(_connectorRepository, _connectorId,
+        _exportDumpConnectionInfo);
+
+    for (final Entry<String, Serializable> entry : extraInformation.entrySet()) {
+      newEntry(ZipConstants.EXTRA_INFO + ZipConstants.PATH_SEPARATOR + entry.getKey());
+      final byte[] byteArray = Util.toByteArray(entry.getValue());
+      _zipOutputStream.write(byteArray);
+      closeEntry();
+    }
+  }
 }
---
+++ src/main/java/de/akquinet/jbosscc/guttenbase/hints/impl/DefaultZipExporterClassResources.java
@@ -0,0 +1,45 @@
+package de.akquinet.jbosscc.guttenbase.hints.impl;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+import de.akquinet.jbosscc.guttenbase.export.zip.ZipExporter;
+import de.akquinet.jbosscc.guttenbase.export.zip.ZipExporterClassResources;
+import de.akquinet.jbosscc.guttenbase.export.zip.ZipStartup;
+
+/**
+ * Default implementation.
+ * 
+ * <p>
+ * &copy; 2012 akquinet tech@spree
+ * </p>
+ * 
+ * @author M. Dahm
+ */
+public class DefaultZipExporterClassResources implements ZipExporterClassResources {
+  @Override
+  public Class<?> getStartupClass() {
+    return ZipStartup.class;
+  }
+
+  @Override
+  public List<Class<?>> getClassResources() {
+    final List<Class<?>> classes = new ArrayList<Class<?>>();
+    classes.add(getStartupClass());
+    classes.add(ZipExporter.class); // Representing all GuttenBase classes
+    classes.add(Logger.class);
+    classes.add(IOUtils.class);
+    return classes;
+  }
+
+  @Override
+  public Map<String, URL> getUrlResources() {
+    return new HashMap<String, URL>();
+  }
+}
