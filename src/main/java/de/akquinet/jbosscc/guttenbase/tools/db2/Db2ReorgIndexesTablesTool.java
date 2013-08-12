4a7b01dbfaeaa1c8d73212abfd32269b7e257296
BWXX470 <BWXX470@bwb.de>
2013-08-12 13:55:25 +0200

    Prevent DB2 SQL Error: SQLCODE=-1146, SQLSTATE=01H52

--- src/main/java/de/akquinet/jbosscc/guttenbase/tools/db2/Db2ReorgIndexesTablesTool.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/tools/db2/Db2ReorgIndexesTablesTool.java
@@ -2,6 +2,7 @@ package de.akquinet.jbosscc.guttenbase.tools.db2;
 
 import java.sql.SQLException;
 
+import de.akquinet.jbosscc.guttenbase.meta.TableMetaData;
 import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
 import de.akquinet.jbosscc.guttenbase.tools.AbstractTablesOperationTool;
 
@@ -23,5 +24,12 @@ public class Db2ReorgIndexesTablesTool extends AbstractTablesOperationTool
   public void executeOnAllTables(final String target) throws SQLException
   {
     executeOnAllTables(target, false, false);
+  }
+
+  @Override
+  public boolean isApplicableOnTable(final TableMetaData tableMetaData)
+  {
+    // Prevent DB2 SQL Error: SQLCODE=-1146, SQLSTATE=01H52
+    return !tableMetaData.getIndexes().isEmpty();
   }
 }
