1ac91b3d4cd743dbff93ed07353cda75662e271a
BWXX470 <BWXX470@bwb.de>
2013-10-02 15:39:55 +0200

    UI

---
+++ src/main/java/de/akquinet/jbosscc/guttenbase/utils/ProgressIndicatorPanel.java
@@ -0,0 +1,130 @@
+package de.akquinet.jbosscc.guttenbase.utils;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+public class ProgressIndicatorPanel extends JPanel
+{
+  private static final long serialVersionUID = 1L;
+  private final JTextField _sourceTable;
+  private final JTextField _targetTable;
+  private final JTextField _totalTime;
+  private final JTextField _tableTime;
+
+  /**
+   * Create the panel.
+   */
+  public ProgressIndicatorPanel()
+  {
+    final GridBagLayout gridBagLayout = new GridBagLayout();
+    gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0 };
+    gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
+    gridBagLayout.columnWeights = new double[] { 0.0, 1.0, 0.0, 0.0, 0, 0.0, Double.MIN_VALUE };
+    gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+    setLayout(gridBagLayout);
+
+    final JLabel lblNewLabel = new JLabel("Source table");
+    lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+    lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+    final GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+    gbc_lblNewLabel.fill = GridBagConstraints.HORIZONTAL;
+    gbc_lblNewLabel.anchor = GridBagConstraints.WEST;
+    gbc_lblNewLabel.insets = new Insets(5, 5, 5, 5);
+    gbc_lblNewLabel.gridx = 0;
+    gbc_lblNewLabel.gridy = 1;
+    add(lblNewLabel, gbc_lblNewLabel);
+
+    _sourceTable = new JTextField();
+    _sourceTable.setEditable(false);
+    final GridBagConstraints gbc_sourceTable = new GridBagConstraints();
+    gbc_sourceTable.gridwidth = 2;
+    gbc_sourceTable.fill = GridBagConstraints.HORIZONTAL;
+    gbc_sourceTable.anchor = GridBagConstraints.WEST;
+    gbc_sourceTable.gridx = 1;
+    gbc_sourceTable.gridy = 1;
+    add(_sourceTable, gbc_sourceTable);
+    _sourceTable.setColumns(40);
+
+    final JLabel lblTargetTable = new JLabel("Target table");
+    lblTargetTable.setFont(new Font("Tahoma", Font.BOLD, 12));
+    final GridBagConstraints gbc_lblTargetTable = new GridBagConstraints();
+    gbc_lblTargetTable.anchor = GridBagConstraints.WEST;
+    gbc_lblTargetTable.insets = new Insets(5, 5, 5, 5);
+    gbc_lblTargetTable.gridx = 3;
+    gbc_lblTargetTable.gridy = 1;
+    add(lblTargetTable, gbc_lblTargetTable);
+
+    _targetTable = new JTextField();
+    _targetTable.setEditable(false);
+    _targetTable.setColumns(40);
+    final GridBagConstraints gbc_targetTable = new GridBagConstraints();
+    gbc_targetTable.weightx = 1.0;
+    gbc_targetTable.anchor = GridBagConstraints.WEST;
+    gbc_targetTable.gridwidth = 2;
+    gbc_targetTable.fill = GridBagConstraints.HORIZONTAL;
+    gbc_targetTable.gridx = 4;
+    gbc_targetTable.gridy = 1;
+    add(_targetTable, gbc_targetTable);
+
+    final JLabel lblTotalTimeElapsed = new JLabel("Total time elapsed");
+    lblTotalTimeElapsed.setFont(new Font("Tahoma", Font.BOLD, 12));
+    final GridBagConstraints gbc_lblTotalTimeElapsed = new GridBagConstraints();
+    gbc_lblTotalTimeElapsed.anchor = GridBagConstraints.WEST;
+    gbc_lblTotalTimeElapsed.insets = new Insets(5, 5, 5, 5);
+    gbc_lblTotalTimeElapsed.gridx = 0;
+    gbc_lblTotalTimeElapsed.gridy = 2;
+    add(lblTotalTimeElapsed, gbc_lblTotalTimeElapsed);
+
+    _totalTime = new JTextField();
+    _totalTime.setEditable(false);
+    _totalTime.setColumns(10);
+    final GridBagConstraints gbc_totalTime = new GridBagConstraints();
+    gbc_totalTime.anchor = GridBagConstraints.WEST;
+    gbc_totalTime.fill = GridBagConstraints.HORIZONTAL;
+    gbc_totalTime.gridx = 1;
+    gbc_totalTime.gridy = 2;
+    add(_totalTime, gbc_totalTime);
+
+    final JLabel lblTableTimeElapsed = new JLabel("Table time elapsed");
+    lblTableTimeElapsed.setFont(new Font("Tahoma", Font.BOLD, 12));
+    final GridBagConstraints gbc_lblTableTimeElapsed = new GridBagConstraints();
+    gbc_lblTableTimeElapsed.anchor = GridBagConstraints.WEST;
+    gbc_lblTableTimeElapsed.insets = new Insets(5, 5, 5, 5);
+    gbc_lblTableTimeElapsed.gridx = 3;
+    gbc_lblTableTimeElapsed.gridy = 2;
+    add(lblTableTimeElapsed, gbc_lblTableTimeElapsed);
+
+    _tableTime = new JTextField();
+    _tableTime.setEditable(false);
+    _tableTime.setColumns(10);
+    final GridBagConstraints gbc_tableTime = new GridBagConstraints();
+    gbc_tableTime.weightx = 1.0;
+    gbc_tableTime.gridwidth = 1;
+    gbc_tableTime.fill = GridBagConstraints.HORIZONTAL;
+    gbc_tableTime.anchor = GridBagConstraints.WEST;
+    gbc_tableTime.gridx = 4;
+    gbc_tableTime.gridy = 2;
+    add(_tableTime, gbc_tableTime);
+
+  }
+
+  public static void main(final String[] args)
+  {
+    final ProgressIndicatorPanel panel = new ProgressIndicatorPanel();
+    final JFrame frame = new JFrame();
+    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    frame.getContentPane().setLayout(new BorderLayout());
+    frame.getContentPane().add(panel, BorderLayout.CENTER);
+    frame.pack();
+    frame.setVisible(true);
+  }
+}
