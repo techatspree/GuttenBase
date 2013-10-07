3f28e5f18b6be9ab6b38d92368c30e4d6daeda6c
BWXX470 <BWXX470@bwb.de>
2013-10-07 09:35:01 +0200

    width

--- src/main/java/de/akquinet/jbosscc/guttenbase/utils/ProgressIndicatorPanel.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/utils/ProgressIndicatorPanel.java
@@ -35,7 +35,7 @@ public class ProgressIndicatorPanel extends JPanel
     final GridBagLayout gridBagLayout = new GridBagLayout();
     gridBagLayout.columnWidths = new int[] { 0, 0, 0, 0, 0, 0, 0 };
     gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0 };
-    gridBagLayout.columnWeights = new double[] { 1.0, 1.0, 0.0, 0.0, 0, 0.0, Double.MIN_VALUE };
+    gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0, 0.0, Double.MIN_VALUE };
     gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
     setLayout(gridBagLayout);
 
@@ -97,6 +97,7 @@ public class ProgressIndicatorPanel extends JPanel
     _totalTime.setEditable(false);
     _totalTime.setColumns(10);
     final GridBagConstraints gbc_totalTime = new GridBagConstraints();
+    gbc_totalTime.weightx = 1.0;
     gbc_totalTime.insets = new Insets(0, 0, 5, 5);
     gbc_totalTime.anchor = GridBagConstraints.WEST;
     gbc_totalTime.fill = GridBagConstraints.HORIZONTAL;
