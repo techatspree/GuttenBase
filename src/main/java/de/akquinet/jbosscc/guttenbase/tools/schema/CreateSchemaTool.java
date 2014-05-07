88cc38b8517197481273dbdd3b253164e37cb136
BWXX470 <BWXX470@bwb.de>
2014-05-07 12:10:04 +0200

    case-sensitive schema tool

--- src/main/java/de/akquinet/jbosscc/guttenbase/tools/schema/CreateSchemaTool.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/tools/schema/CreateSchemaTool.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.akquinet.jbosscc.guttenbase.hints.CaseConversionMode;
 import de.akquinet.jbosscc.guttenbase.meta.DatabaseMetaData;
 import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
 import de.akquinet.jbosscc.guttenbase.tools.ScriptExecutorTool;
@@ -16,20 +17,26 @@ import de.akquinet.jbosscc.guttenbase.tools.ScriptExecutorTool;
 public class CreateSchemaTool
 {
   private final ConnectorRepository _connectorRepository;
-  private SchemaColumnTypeMapper _schemaColumnTypeMapper;
+  private final SchemaColumnTypeMapper _schemaColumnTypeMapper;
+  private final CaseConversionMode _caseConversionMode;
 
-  public CreateSchemaTool(final ConnectorRepository connectorRepository, SchemaColumnTypeMapper schemaColumnTypeMapper)
+  public CreateSchemaTool(
+      final ConnectorRepository connectorRepository,
+      final SchemaColumnTypeMapper schemaColumnTypeMapper,
+      final CaseConversionMode caseConversionMode)
   {
+    assert caseConversionMode != null : "caseConversionMode != null";
     assert connectorRepository != null : "connectorRepository != null";
     assert schemaColumnTypeMapper != null : "schemaColumnTypeMapper != null";
 
     _connectorRepository = connectorRepository;
     _schemaColumnTypeMapper = schemaColumnTypeMapper;
+    _caseConversionMode = caseConversionMode;
   }
 
   public CreateSchemaTool(final ConnectorRepository connectorRepository)
   {
-    this(connectorRepository, new DefaultSchemaColumnTypeMapper());
+    this(connectorRepository, new DefaultSchemaColumnTypeMapper(), CaseConversionMode.NONE);
   }
 
   public List<String> createDDLScript(final String connectorId, final String schema) throws SQLException
@@ -37,7 +44,8 @@ public class CreateSchemaTool
     final List<String> result = new ArrayList<String>();
     final DatabaseMetaData databaseMetaData = _connectorRepository.getDatabaseMetaData(connectorId);
 
-    final DatabaseSchemaScriptCreator databaseSchemaScriptCreator = new DatabaseSchemaScriptCreator(databaseMetaData, schema);
+    final DatabaseSchemaScriptCreator databaseSchemaScriptCreator = new DatabaseSchemaScriptCreator(databaseMetaData, schema,
+        _caseConversionMode);
     databaseSchemaScriptCreator.setColumnTypeMapper(_schemaColumnTypeMapper);
     result.addAll(databaseSchemaScriptCreator.createTableStatements());
     result.addAll(databaseSchemaScriptCreator.createPrimaryKeyStatements());
