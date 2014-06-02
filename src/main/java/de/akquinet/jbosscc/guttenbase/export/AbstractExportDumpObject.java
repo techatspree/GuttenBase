71f3c6ac3cb2c8b240eba4f2c744ab7708bc961d
BWXX470 <BWXX470@bwb.de>
2014-06-02 15:43:46 +0200

    reuse temp file to reduce open file issue

--- src/main/java/de/akquinet/jbosscc/guttenbase/export/AbstractExportDumpObject.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/export/AbstractExportDumpObject.java
@@ -1,115 +1,130 @@
 package de.akquinet.jbosscc.guttenbase.export;
 
-import java.io.Externalizable;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+import java.io.*;
 import java.sql.SQLException;
 
 /**
  * Since CLOBs/BLOBs may be quite big. we do not load them into memory
  * completely, but read them in chunks and write the data to the output stream
  * in a loop.
- *
+ * <p/>
  * <p>
  * &copy; 2012-2020 akquinet tech@spree
  * </p>
  *
  * @author M. Dahm
  */
-public abstract class AbstractExportDumpObject implements Externalizable {
-	private static final long serialVersionUID = 1L;
+public abstract class AbstractExportDumpObject implements Externalizable
+{
+  private static final long serialVersionUID = 1L;
 
-	public static final int DEFAULT_BUFFER_SIZE = 1024 * 1024 * 10;
+  public static final int DEFAULT_BUFFER_SIZE = 1024 * 1024 * 10;
 
-	private transient File _tempFile;
+  private static transient File _tempFile;
 
-	private transient final InputStream _inputStream;
+  private transient final InputStream _inputStream;
 
-	public AbstractExportDumpObject() {
-		this(null);
-	}
+  public AbstractExportDumpObject()
+  {
+    this(null);
+  }
 
-	public AbstractExportDumpObject(final InputStream inputStream) {
-		_inputStream = inputStream;
-	}
+  public AbstractExportDumpObject(final InputStream inputStream)
+  {
+    _inputStream = inputStream;
+  }
 
-	/**
-	 * Read data in chunks and write it to the outputstream to avoid out of memory
-	 * errors.
-	 */
-	@Override
-	public void writeExternal(final ObjectOutput output) throws IOException {
-		final byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
+  /**
+   * Read data in chunks and write it to the outputstream to avoid out of memory
+   * errors.
+   */
+  @Override
+  public void writeExternal(final ObjectOutput output) throws IOException
+  {
+    final byte[] buffer = new byte[DEFAULT_BUFFER_SIZE];
 
-		for (int n = _inputStream.read(buffer); n > 0; n = _inputStream.read(buffer)) {
-			byte[] buf = buffer;
+    for (int n = _inputStream.read(buffer); n > 0; n = _inputStream.read(buffer))
+    {
+      byte[] buf = buffer;
 
-			if (n < DEFAULT_BUFFER_SIZE) {
-				buf = new byte[n];
-				System.arraycopy(buffer, 0, buf, 0, n);
-			}
+      if (n < DEFAULT_BUFFER_SIZE)
+      {
+        buf = new byte[n];
+        System.arraycopy(buffer, 0, buf, 0, n);
+      }
 
-			output.writeObject(buf);
-			output.flush();
-		}
+      output.writeObject(buf);
+      output.flush();
+    }
 
-		output.writeObject(null);
-	}
+    output.writeObject(null);
+  }
 
-	/**
-	 * Store read data in temporary file to avoid out of memory errors.
-	 */
-	@Override
-	public void readExternal(final ObjectInput input) throws IOException, ClassNotFoundException {
-		_tempFile = File.createTempFile("GB-DUMP-", null);
-		_tempFile.deleteOnExit();
-		final FileOutputStream fileOutputStream = new FileOutputStream(_tempFile);
+  /**
+   * Store read data in temporary file to avoid out of memory errors.
+   */
+  @Override
+  public void readExternal(final ObjectInput input) throws IOException, ClassNotFoundException
+  {
+    if (_tempFile == null)
+    {
+      _tempFile = File.createTempFile("GB-DUMP-", null);
+      _tempFile.deleteOnExit();
+    }
+    final FileOutputStream fileOutputStream = new FileOutputStream(_tempFile);
 
-		for (byte[] buffer = (byte[]) input.readObject(); buffer != null; buffer = (byte[]) input.readObject()) {
-			fileOutputStream.write(buffer, 0, buffer.length);
-		}
+    for (byte[] buffer = (byte[]) input.readObject(); buffer != null; buffer = (byte[]) input.readObject())
+    {
+      fileOutputStream.write(buffer, 0, buffer.length);
+    }
 
-		fileOutputStream.close();
-	}
+    fileOutputStream.close();
+  }
 
-	public long length() throws SQLException {
-		return _tempFile.length();
-	}
+  public long length() throws SQLException
+  {
+    return _tempFile.length();
+  }
 
-	public byte[] getBytes(final long pos, final int length) throws SQLException {
-		try {
-			final InputStream inputStream = getBinaryStream(pos, length);
-			final byte[] bytes = new byte[length];
-			inputStream.read(bytes);
+  public byte[] getBytes(final long pos, final int length) throws SQLException
+  {
+    try
+    {
+      final InputStream inputStream = getBinaryStream(pos, length);
+      final byte[] bytes = new byte[length];
+      inputStream.read(bytes);
       inputStream.close();
 
-			return bytes;
-		} catch (final IOException e) {
-			throw new SQLException("getBytes", e);
-		}
-	}
+      return bytes;
+    }
+    catch (final IOException e)
+    {
+      throw new SQLException("getBytes", e);
+    }
+  }
 
-	public InputStream getBinaryStream() throws SQLException {
-		return getBinaryStream(0, length());
-	}
+  public InputStream getBinaryStream() throws SQLException
+  {
+    return getBinaryStream(0, length());
+  }
 
-	public InputStream getBinaryStream(final long pos, final long length) throws SQLException {
-		try {
-			final FileInputStream inputStream = new FileInputStream(_tempFile);
-			inputStream.skip(pos);
-			return inputStream;
-		} catch (final IOException e) {
-			throw new SQLException("getBinaryStream", e);
-		}
-	}
+  public InputStream getBinaryStream(final long pos, final long length) throws SQLException
+  {
+    try
+    {
+      final FileInputStream inputStream = new FileInputStream(_tempFile);
+      inputStream.skip(pos);
+      return inputStream;
+    }
+    catch (final IOException e)
+    {
+      throw new SQLException("getBinaryStream", e);
+    }
+  }
 
-	public void free() throws SQLException {
-		_tempFile.delete();
-		_tempFile = null;
-	}
+  public void free() throws SQLException
+  {
+    _tempFile.delete();
+    _tempFile = null;
+  }
 }
