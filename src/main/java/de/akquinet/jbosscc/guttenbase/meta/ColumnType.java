887a578916f4d15d62e50435b0a3eec77cedd06b
BWXX470 <BWXX470@bwb.de>
2014-06-10 11:12:43 +0200

    cleanup

--- src/main/java/de/akquinet/jbosscc/guttenbase/meta/ColumnType.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/meta/ColumnType.java
@@ -1,5 +1,9 @@
 package de.akquinet.jbosscc.guttenbase.meta;
 
+import de.akquinet.jbosscc.guttenbase.connector.DatabaseType;
+import de.akquinet.jbosscc.guttenbase.exceptions.UnhandledColumnTypeException;
+import de.akquinet.jbosscc.guttenbase.utils.Util;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.math.BigInteger;
@@ -17,16 +21,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import de.akquinet.jbosscc.guttenbase.connector.DatabaseType;
-import de.akquinet.jbosscc.guttenbase.exceptions.UnhandledColumnTypeException;
-import de.akquinet.jbosscc.guttenbase.utils.Util;
-
 /**
  * Define column type and mapping methods
  * <p>
  * &copy; 2012-2020 akquinet tech@spree
  * </p>
- * 
+ *
  * @author M. Dahm
  */
 public enum ColumnType
@@ -75,43 +75,42 @@ public enum ColumnType
   }
 
   private Object getValueFromResultset(final ResultSet resultSet, final int columnIndex)
-      throws SQLException,
-      UnhandledColumnTypeException
+          throws SQLException
   {
     switch (this)
     {
-    case CLASS_STRING:
-      return resultSet.getString(columnIndex);
-    case CLASS_DOUBLE:
-      return resultSet.getDouble(columnIndex);
-    case CLASS_INTEGER:
-      return resultSet.getInt(columnIndex);
-    case CLASS_LONG:
-      return resultSet.getLong(columnIndex);
-    case CLASS_BLOB:
-      return resultSet.getBlob(columnIndex);
-    case CLASS_CLOB:
-      return resultSet.getClob(columnIndex);
-    case CLASS_SQLXML:
-      return resultSet.getSQLXML(columnIndex);
-    case CLASS_FLOAT:
-      return resultSet.getFloat(columnIndex);
-    case CLASS_BOOLEAN:
-      return resultSet.getBoolean(columnIndex);
-    case CLASS_BIGDECIMAL:
-      return resultSet.getBigDecimal(columnIndex);
-    case CLASS_TIMESTAMP:
-      return resultSet.getTimestamp(columnIndex);
-    case CLASS_DATE:
-      return resultSet.getDate(columnIndex);
-    case CLASS_SHORT:
-      return resultSet.getShort(columnIndex);
-    case CLASS_TIME:
-      return resultSet.getTime(columnIndex);
-    case CLASS_OBJECT:
-      return resultSet.getObject(columnIndex);
-    default:
-      throw new UnhandledColumnTypeException("Unhandled column type (" + this + ")");
+      case CLASS_STRING:
+        return resultSet.getString(columnIndex);
+      case CLASS_DOUBLE:
+        return resultSet.getDouble(columnIndex);
+      case CLASS_INTEGER:
+        return resultSet.getInt(columnIndex);
+      case CLASS_LONG:
+        return resultSet.getLong(columnIndex);
+      case CLASS_BLOB:
+        return resultSet.getBlob(columnIndex);
+      case CLASS_CLOB:
+        return resultSet.getClob(columnIndex);
+      case CLASS_SQLXML:
+        return resultSet.getSQLXML(columnIndex);
+      case CLASS_FLOAT:
+        return resultSet.getFloat(columnIndex);
+      case CLASS_BOOLEAN:
+        return resultSet.getBoolean(columnIndex);
+      case CLASS_BIGDECIMAL:
+        return resultSet.getBigDecimal(columnIndex);
+      case CLASS_TIMESTAMP:
+        return resultSet.getTimestamp(columnIndex);
+      case CLASS_DATE:
+        return resultSet.getDate(columnIndex);
+      case CLASS_SHORT:
+        return resultSet.getShort(columnIndex);
+      case CLASS_TIME:
+        return resultSet.getTime(columnIndex);
+      case CLASS_OBJECT:
+        return resultSet.getObject(columnIndex);
+      default:
+        throw new UnhandledColumnTypeException("Unhandled column type (" + this + ")");
     }
   }
 
@@ -119,7 +118,7 @@ public enum ColumnType
    * Set value in {@link PreparedStatement}
    */
   public void setValue(final PreparedStatement insertStatement, final int columnIndex, final Object data,
-      final DatabaseType databaseType, final int sqlType) throws SQLException
+                       final DatabaseType databaseType, final int sqlType) throws SQLException
   {
     if (data == null)
     {
@@ -132,71 +131,71 @@ public enum ColumnType
   }
 
   private void setStatementValue(final PreparedStatement insertStatement, final int columnIndex, final Object data,
-      final DatabaseType databaseType) throws SQLException, UnhandledColumnTypeException
+                                 final DatabaseType databaseType) throws SQLException
   {
     switch (this)
     {
-    case CLASS_STRING:
-      insertStatement.setString(columnIndex, (String) data);
-      break;
-    case CLASS_INTEGER:
-      insertStatement.setInt(columnIndex, (Integer) data);
-      break;
-    case CLASS_LONG:
-      insertStatement.setLong(columnIndex, (Long) data);
-      break;
-    case CLASS_DOUBLE:
-      insertStatement.setDouble(columnIndex, (Double) data);
-      break;
-    case CLASS_BLOB:
-      if (driverSupportsStream(databaseType))
-      {
-        insertStatement.setBlob(columnIndex, ((Blob) data).getBinaryStream());
-      }
-      else
-      {
-        insertStatement.setBlob(columnIndex, ((Blob) data));
-      }
-      break;
-    case CLASS_CLOB:
-      if (driverSupportsStream(databaseType))
-      {
-        insertStatement.setClob(columnIndex, ((Clob) data).getCharacterStream());
-      }
-      else
-      {
-        insertStatement.setClob(columnIndex, ((Clob) data));
-      }
-      break;
-    case CLASS_SQLXML:
-      insertStatement.setSQLXML(columnIndex, (SQLXML) data);
-      break;
-    case CLASS_BOOLEAN:
-      insertStatement.setBoolean(columnIndex, (Boolean) data);
-      break;
-    case CLASS_BIGDECIMAL:
-      insertStatement.setBigDecimal(columnIndex, (BigDecimal) data);
-      break;
-    case CLASS_TIMESTAMP:
-      insertStatement.setTimestamp(columnIndex, (Timestamp) data);
-      break;
-    case CLASS_DATE:
-      insertStatement.setDate(columnIndex, (Date) data);
-      break;
-    case CLASS_FLOAT:
-      insertStatement.setFloat(columnIndex, (Float) data);
-      break;
-    case CLASS_SHORT:
-      insertStatement.setShort(columnIndex, (Short) data);
-      break;
-    case CLASS_TIME:
-      insertStatement.setTime(columnIndex, (Time) data);
-      break;
-    case CLASS_OBJECT:
-      insertStatement.setObject(columnIndex, data);
-      break;
-    default:
-      throw new UnhandledColumnTypeException("Unhandled column type (" + this + ")");
+      case CLASS_STRING:
+        insertStatement.setString(columnIndex, (String) data);
+        break;
+      case CLASS_INTEGER:
+        insertStatement.setInt(columnIndex, (Integer) data);
+        break;
+      case CLASS_LONG:
+        insertStatement.setLong(columnIndex, (Long) data);
+        break;
+      case CLASS_DOUBLE:
+        insertStatement.setDouble(columnIndex, (Double) data);
+        break;
+      case CLASS_BLOB:
+        if (driverSupportsStream(databaseType))
+        {
+          insertStatement.setBlob(columnIndex, ((Blob) data).getBinaryStream());
+        }
+        else
+        {
+          insertStatement.setBlob(columnIndex, ((Blob) data));
+        }
+        break;
+      case CLASS_CLOB:
+        if (driverSupportsStream(databaseType))
+        {
+          insertStatement.setClob(columnIndex, ((Clob) data).getCharacterStream());
+        }
+        else
+        {
+          insertStatement.setClob(columnIndex, ((Clob) data));
+        }
+        break;
+      case CLASS_SQLXML:
+        insertStatement.setSQLXML(columnIndex, (SQLXML) data);
+        break;
+      case CLASS_BOOLEAN:
+        insertStatement.setBoolean(columnIndex, (Boolean) data);
+        break;
+      case CLASS_BIGDECIMAL:
+        insertStatement.setBigDecimal(columnIndex, (BigDecimal) data);
+        break;
+      case CLASS_TIMESTAMP:
+        insertStatement.setTimestamp(columnIndex, (Timestamp) data);
+        break;
+      case CLASS_DATE:
+        insertStatement.setDate(columnIndex, (Date) data);
+        break;
+      case CLASS_FLOAT:
+        insertStatement.setFloat(columnIndex, (Float) data);
+        break;
+      case CLASS_SHORT:
+        insertStatement.setShort(columnIndex, (Short) data);
+        break;
+      case CLASS_TIME:
+        insertStatement.setTime(columnIndex, (Time) data);
+        break;
+      case CLASS_OBJECT:
+        insertStatement.setObject(columnIndex, data);
+        break;
+      default:
+        throw new UnhandledColumnTypeException("Unhandled column type (" + this + ")");
     }
   }
 
@@ -240,7 +239,7 @@ public enum ColumnType
   /**
    * Check if class can be mapped to {@link ColumnType}.
    */
-  public static boolean isSupportedClass(final Class<?> columnClass) throws SQLException
+  public static boolean isSupportedClass(final Class<?> columnClass)
   {
     init();
 
