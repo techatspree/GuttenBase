6854d481ccd3ee2419e199ec73d66a0f5b8cd65b
BWXX470 <BWXX470@bwb.de>
2015-06-19 09:44:36 +0200

    override if wanted

--- src/main/java/de/akquinet/jbosscc/guttenbase/configuration/impl/DefaultTargetDatabaseConfiguration.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/configuration/impl/DefaultTargetDatabaseConfiguration.java
@@ -1,45 +1,39 @@
 package de.akquinet.jbosscc.guttenbase.configuration.impl;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
 import de.akquinet.jbosscc.guttenbase.configuration.TargetDatabaseConfiguration;
 import de.akquinet.jbosscc.guttenbase.meta.TableMetaData;
 import de.akquinet.jbosscc.guttenbase.repository.ConnectorRepository;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * (Almost) empty implementation
- * 
+ * <p/>
  * <p>
  * &copy; 2012-2020 akquinet tech@spree
  * </p>
- * 
+ *
  * @author M. Dahm
  */
 public abstract class DefaultTargetDatabaseConfiguration extends AbstractDatabaseConfiguration implements TargetDatabaseConfiguration {
-  private boolean _mayCommit = true;
-
   public DefaultTargetDatabaseConfiguration(final ConnectorRepository connectorRepository) {
     super(connectorRepository);
   }
 
   /**
    * {@inheritDoc}
-   * 
+   * <p/>
    * true by default
    */
   @Override
   public boolean isMayCommit() {
-    return _mayCommit;
-  }
-
-  public void setMayCommit(final boolean mayCommit) {
-    _mayCommit = mayCommit;
+    return true;
   }
 
   /**
    * Connection is set autocommit false.
-   * 
+   * <p/>
    * {@inheritDoc}
    */
   @Override
