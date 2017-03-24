585b50ede9103af9f278fe7653d99421232d5bfd
Markus Dahm <markus.dahm@akquinet.de>
2017-03-24 11:20:01 +0100

    JDK 1.8

--- src/main/java/de/akquinet/jbosscc/guttenbase/hints/ConnectorHint.java
+++ src/main/java/de/akquinet/jbosscc/guttenbase/hints/ConnectorHint.java
@@ -3,15 +3,15 @@ package de.akquinet.jbosscc.guttenbase.hints;
 /**
  * Users may add configuration "hints" that influence the tools. E.g., the buffer size when reading or writing data. There is always a
  * default implementation added to a connector by the repository which may be overridden subsequently.
- * 
+ * <p/>
  * <p>
  * &copy; 2012-2020 akquinet tech@spree
  * </p>
- * 
+ *
  * @author M. Dahm
  */
 public interface ConnectorHint<T> {
-	Class<T> getConnectorHintType();
+    Class<T> getConnectorHintType();
 
-	T getValue();
+    T getValue();
 }
